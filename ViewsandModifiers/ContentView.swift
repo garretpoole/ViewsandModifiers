875d048277a62bfeb683144a03ecacba8d5f2707
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-13 13:05:12 -0800

    containers example grid

--- ViewsandModifiers/ContentView.swift
+++ ViewsandModifiers/ContentView.swift
@@ -6,54 +6,36 @@
 //
 
 import SwiftUI
-
-//custom modifier for titles in the project
-struct Title: ViewModifier{
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-//extend so in main body can just use '.titleStyle()'
-extension View{
-    func titleStyle() -> some View{
-        modifier(Title())
-    }
-}
-//custom ViewModifiers can have custom properties
-struct Watermark: ViewModifier{
-    var text: String
-    func body(content: Content) -> some View{
-        ZStack(alignment: .bottomTrailing){
-            content
-            
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding()
-                .background(.black)
+//Generics
+//can pass any kind of content that must conform to the View protocol
+struct GridStack<Content: View>: View{
+    let rows: Int
+    let columns: Int
+    //defines a function that accepts two ints and returns content to display
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View{
+        VStack{
+            //without 'id: ...' assumes rows and columns are fixed
+            ForEach(0..<rows, id: \.self){ row in
+                HStack{
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-    }
-}
-//do not have ability to have custom properties
-extension View{
-    func watermarked(with text: String) -> some View{
-        modifier(Watermark(text: text))
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
-            .titleStyle()
-        Color.blue
-            .frame(width: 300, height: 300)
-            .watermarked(with: "Hacking with Swift")
-        
+        GridStack(rows: 4, columns: 4) { row, col in
+       
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+            
+        }
     }
 }
 
