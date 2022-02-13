0a2cbae93ffe4f9b1b9ad5c6701611ee359b1402
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-13 12:12:25 -0800

    modifier order matters

--- ViewsandModifiers/ContentView.swift
+++ ViewsandModifiers/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //modifiers order matters
+        //creates a struct with the color and then wraps frame
+        //therefore color may not fill frame based on order
+        Button("Hello World") {
+            print(type(of: self.body))
+        }
+        .frame(width: 200, height: 200)
+        .background(.red)
     }
 }
 
