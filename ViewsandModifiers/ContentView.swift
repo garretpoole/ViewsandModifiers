5c53430d2caa134b19d816746e2540f93ecd86c3
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-13 12:32:11 -0800

    environment modifiers

--- ViewsandModifiers/ContentView.swift
+++ ViewsandModifiers/ContentView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var useRedText = false
-    
     var body: some View {
-        //prefer ternary conditional operator ?
-        //two if statements makes swiftui do a lot more work
-        Button("Hello World") {
-            useRedText.toggle()
+        //child modifier is priority over the parent modifier
+        VStack{
+            Text("Gryffindor")
+                .font(.largeTitle)
+                .blur(radius: 0)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
+                .blur(radius: 5)
         }
-        .foregroundColor(useRedText ? .red : .blue)
+        //environment (parent) modifier applies to everything inside
+        .font(.title)
+        //certain modifiers are not environment, they are cumulaive
+        .blur(radius: 5)
         
     }
 }
