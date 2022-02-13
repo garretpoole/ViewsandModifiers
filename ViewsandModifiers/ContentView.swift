7a7eb59b9b45476f46881307187dcf769e17afb6
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-13 12:44:39 -0800

    view composition; using views as structs for reusability

--- ViewsandModifiers/ContentView.swift
+++ ViewsandModifiers/ContentView.swift
@@ -7,25 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    //views as properties
-    let motto1 = Text("Draco dormiens")
-    let motto2 = Text("nunquam titillandus")
-    //ViewBuilder wraps interior like body does
-    @ViewBuilder var spells: some View{
-        Text("Lumos")
-        Text("Obliviate")
+struct CapsuleText: View{
+    var text: String
+    
+    var body: some View{
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            //.foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
     }
+}
+
+struct ContentView: View {
     var body: some View {
-        VStack{
-            motto1
-                .foregroundColor(.red)
-            motto2
-                .foregroundColor(.blue)
-        }
-        
-        HStack{
-            spells
+        VStack(spacing: 10){
+            CapsuleText(text: "First")
+                .foregroundColor(.white)
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
         }
         
     }
