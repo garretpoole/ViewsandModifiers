0933498e5ce219d13a34d9b8eddc69abd02dd7ff
Garret Poole <garretpoole@users.noreply.github.com>
2022-02-13 12:55:48 -0800

    custom modifiers using ViewModifer

--- ViewsandModifiers/ContentView.swift
+++ ViewsandModifiers/ContentView.swift
@@ -7,27 +7,52 @@
 
 import SwiftUI
 
-struct CapsuleText: View{
-    var text: String
-    
-    var body: some View{
-        Text(text)
+//custom modifier for titles in the project
+struct Title: ViewModifier{
+    func body(content: Content) -> some View {
+        content
             .font(.largeTitle)
+            .foregroundColor(.white)
             .padding()
-            //.foregroundColor(.white)
             .background(.blue)
-            .clipShape(Capsule())
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+//extend so in main body can just use '.titleStyle()'
+extension View{
+    func titleStyle() -> some View{
+        modifier(Title())
+    }
+}
+//custom ViewModifiers can have custom properties
+struct Watermark: ViewModifier{
+    var text: String
+    func body(content: Content) -> some View{
+        ZStack(alignment: .bottomTrailing){
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding()
+                .background(.black)
+        }
+    }
+}
+//do not have ability to have custom properties
+extension View{
+    func watermarked(with text: String) -> some View{
+        modifier(Watermark(text: text))
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10){
-            CapsuleText(text: "First")
-                .foregroundColor(.white)
-            CapsuleText(text: "Second")
-                .foregroundColor(.yellow)
-        }
+        Text("Hello World")
+            .titleStyle()
+        Color.blue
+            .frame(width: 300, height: 300)
+            .watermarked(with: "Hacking with Swift")
         
     }
 }
